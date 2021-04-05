6c1fe91aadcc26fbb686c4f352f504358a1b01c5
Shaker1664 <shakerkamal@iut-dhaka.edu>
2021-04-06 01:31:15 +0600

    Bug fixes on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
         <img src={avatar} alt="" />
       </a>
       <h1>
-        <strong>I am Shaker </strong>, a motivated <br />
+        <strong>I am Shaker </strong>, a passionate <br />
         Software Engineer <br /> trying build a better future by <br />
         solving real-world problems.
       </h1>
